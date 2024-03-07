9b70167f521693fdefc97d29bf8c480b9ab2dc09
Nathan Barta <71276691+NathanBarta@users.noreply.github.com>
2024-03-07 15:16:56 -0500

    Initial Commit

---
+++ VulcanizeUSB/VulcanizeUSBApp.swift
@@ -0,0 +1,17 @@
+//
+//  VulcanizeUSBApp.swift
+//  VulcanizeUSB
+//
+//  Created by Nathan Barta on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct VulcanizeUSBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
